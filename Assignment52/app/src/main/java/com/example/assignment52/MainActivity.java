7b0b2276d4f3cfc08f56a5d12759c3c366ce0c86
pqhieu <quanghieu221002@gmail.com>
2023-11-24 11:50:00 +0200

    fifth commit

--- Assignment52/app/src/main/java/com/example/assignment52/MainActivity.java
+++ Assignment52/app/src/main/java/com/example/assignment52/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.assignment52;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
@@ -17,9 +23,7 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> firstNames = new ArrayList<>();
-    private ArrayList<String> lastNames = new ArrayList<>();
-    private ArrayList<String> phoneNumbers = new ArrayList<>();
+    private ArrayList<String> data = new ArrayList<>();
 
     private AutoCompleteTextView actvSearchFirstName;
     private AutoCompleteTextView actvSearchLastName;
@@ -33,39 +37,158 @@ public class MainActivity extends AppCompatActivity {
         final EditText etFirstName = findViewById(R.id.etFirstName);
         final EditText etLastName = findViewById(R.id.etLastName);
         final EditText etPhoneNumber = findViewById(R.id.etPhoneNumber);
-        Button btnSave = findViewById(R.id.btnSave);
+        final Button btnSave = findViewById(R.id.btnSave);
+        final Button btnClear = findViewById(R.id.btnClear);
 
         actvSearchFirstName = findViewById(R.id.actvSearchFirstName);
         actvSearchLastName = findViewById(R.id.actvSearchLastName);
         actvSearchPhoneNumber = findViewById(R.id.actvSearchPhoneNumber);
 
-        ArrayAdapter<String> firstNameAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_dropdown_item_1line, firstNames);
+        ArrayAdapter<String> firstNameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, data) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                String item = getItem(position);
+                if (item != null || item.isEmpty()) {
+                    String[] parts = item.split(":");
+                    if (parts.length > 1) {
+                        textView.setText(parts[0].trim());
+                    }
+                }
+
+                return view;
+            }
+        };
         actvSearchFirstName.setAdapter(firstNameAdapter);
 
-        ArrayAdapter<String> lastNameAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_dropdown_item_1line, lastNames);
+        ArrayAdapter<String> lastNameAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_dropdown_item_1line, data) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                String item = getItem(position);
+                if (item != null || item.isEmpty()) {
+                    String[] parts = item.split(":");
+                    if (parts.length > 1) {
+                        textView.setText(parts[0].trim());
+                    }
+                }
+
+                return view;
+            }
+        };
         actvSearchLastName.setAdapter(lastNameAdapter);
 
-        ArrayAdapter<String> phoneNumberAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_dropdown_item_1line, phoneNumbers);
+        ArrayAdapter<String> phoneNumberAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_dropdown_item_1line, data){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                String item = getItem(position);
+                if (item != null || item.isEmpty()) {
+                    String[] parts = item.split(":");
+                    if (parts.length > 1) {
+                        textView.setText(parts[0].trim());
+                    }
+                }
+
+                return view;
+            }
+        };
         actvSearchPhoneNumber.setAdapter(phoneNumberAdapter);
+        actvSearchFirstName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                String[] parts = selection.split(":");
+                String firstName = parts[1];
+                String lastName = parts[2];
+                String phone = parts[3];
+
+                actvSearchFirstName.setText(firstName);
+                actvSearchLastName.setText(lastName);
+                actvSearchPhoneNumber.setText(phone);
+            }
+        });
+
+        actvSearchLastName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                String[] parts = selection.split(":");
+                String firstName = parts[1];
+                String lastName = parts[2];
+                String phone = parts[3];
+
+                actvSearchFirstName.setText(firstName);
+                actvSearchLastName.setText(lastName);
+                actvSearchPhoneNumber.setText(phone);
+            }
+        });
+
+        actvSearchPhoneNumber.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                String[] parts = selection.split(":");
+                String firstName = parts[1];
+                String lastName = parts[2];
+                String phone = parts[3];
+
+                actvSearchFirstName.setText(firstName);
+                actvSearchLastName.setText(lastName);
+                actvSearchPhoneNumber.setText(phone);
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actvSearchFirstName.setText("");
+                actvSearchLastName.setText("");
+                actvSearchPhoneNumber.setText("");
+            }
+        });
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String firstName = etFirstName.getText().toString();
-                String lastName = etLastName.getText().toString();
-                String phoneNumber = etPhoneNumber.getText().toString();
+                firstNameAdapter.add(
+                        etFirstName.getText().toString() + ":" +
+                        etFirstName.getText().toString() + ":" +
+                        etLastName.getText().toString() + ":" +
+                        etPhoneNumber.getText().toString()
+                );
+                lastNameAdapter.add(
+                        etLastName.getText().toString() + ":" +
+                        etFirstName.getText().toString() + ":" +
+                        etLastName.getText().toString() + ":" +
+                        etPhoneNumber.getText().toString()
+                );
+                phoneNumberAdapter.add(
+                        etPhoneNumber.getText().toString() + ":" +
+                        etFirstName.getText().toString() + ":" +
+                        etLastName.getText().toString() + ":" +
+                        etPhoneNumber.getText().toString()
+                );
 
                 etFirstName.setText("");
                 etLastName.setText("");
                 etPhoneNumber.setText("");
-
-                firstNameAdapter.add(firstName);
-                lastNameAdapter.add(lastName);
-                phoneNumberAdapter.add(phoneNumber);
             }
         });
+    }
+
+    public static void autofill(AutoCompleteTextView autoCompleteTextView, String text) {
+        autoCompleteTextView.setText(text);
     }
 }
